2b295d7e9b5f04332277e0fbc7315e4ac3f3748c
Michael Miller <Makuna@hotmail.com>
2016-11-02 18:13:47 -0700

    Using commands to control LEDs

--- PandorasChest.ino
+++ PandorasChest.ino
@@ -1,16 +1,37 @@
 #include <ESP8266WiFi.h>
 #include <PandoraCommunications.h>
+#include <AnalogKeypad.h>
 
 const char* ssid = "AP001";
 const char* password = "ItIsABadDay";
+
+const uint8_t c_ledHighPower = D1; // using esp8266 board pin, AKA GPIO5, 
 
 PandoraServer book;
 
 void OnBookCommand(const BookCommand& command)
 {
+    
   Serial.print(command.command);
   Serial.print(" ");
   Serial.println(command.param);
+  
+  if (command.param == ButtonState_Click)
+  {
+    switch (command.command)
+    {
+      case ButtonId_1:
+      // toggle built in LED
+      digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) == HIGH ? LOW : HIGH); // off
+      break;
+
+      case ButtonId_2:
+      // toggle High Power LED
+      digitalWrite(c_ledHighPower, digitalRead(c_ledHighPower) == HIGH ? LOW : HIGH); // off
+      
+    }
+  }
+
 }
 
 void setup() {
@@ -18,6 +39,14 @@ void setup() {
   while (!Serial); // wait for serial attach
   Serial.println();
 
+  // setup onboard LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // off
+
+  // setup external high power LED
+  pinMode(c_ledHighPower, OUTPUT);
+  digitalWrite(c_ledHighPower, LOW); // off
+  
   book.begin(ssid, password);
 }
 
